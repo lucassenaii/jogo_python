08e261a1eb4f3356d0da70c1465519ec8ffe0f06
lucassenaii@gmail.com <lucassenaii@gmail.comgit config --global user.email lucassenaii@gmail.comgit config --global lucassenaiigit config --global lucassenaii>
2024-10-29 18:56:58 -0300

    dois jogadores

--- jogo_da_velha.py
+++ jogo_da_velha.py
@@ -14,10 +14,14 @@ fonte_quadrinhos = pygame.font.SysFont('Comic Sans Ms', 100, True, True) #import
 running = True #variável de controle do status do jogo
 
 personagem_x = fonte_quadrinhos.render('X', True, 'red')
-personagem_y = fonte_quadrinhos.render('O', True, 'red')
-apresenta_personagem = 0
-x = 0
-y = 0
+personagem_o = fonte_quadrinhos.render('O', True, 'red')
+
+jogador_atual = personagem_x #inicializa o jogo com o X
+
+rodadas = 0
+
+coordenada_x = 0
+coordenada_y = 0
 
 while running:
     # controle de enventos no jgo
@@ -28,51 +32,61 @@ while running:
         # pygame.MOUSEBUTTONDOWN significa evento de click do mouse
         if event.type == pygame.MOUSEBUTTONDOWN:
             print('Clicou')
-            click_pos = pygame.mouse.get_pos() #a posiçao do mouse quando houver evento de clique
-            print('eixo x:', click_pos[0])
-            print('eixo y:', click_pos[1])
-            x = click_pos[0]
-            y = click_pos[1]
-            apresenta_personagem = apresenta_personagem + 1
-            if(apresenta_personagem >= 10):
-                screen.fill('black')
-                apresenta_personagem = 0
-    
+            click_pos = pygame.mouse.get_pos() # a posição do mouse quando houve o evento de click
+            print('eixo X:', click_pos[0])
+            print('eixo Y:', click_pos[1])
+            coordenada_x = click_pos[0]
+            coordenada_y = click_pos[1]
+            rodadas = rodadas + 1
+            if rodadas >= 10:
+                screen.fill('black') 
+                rodadas = 0
+                coordenada_x = 0
+                coordenada_y = 0
+                
+            if rodadas != 1:
+                if jogador_atual == personagem_x:
+                    jogador_atual = personagem_o
+                else:
+                    jogador_atual = personagem_x
+            else:
+                jogador_atual = personagem_x
     #Desenha tabuleiro
-    #                                 origem      destino    
+    #                                  origem      destino    
     #                                ( x , y)   ( x , y ) 
     pygame.draw.line(screen, 'blue',(200, 0), (200, 600), 10)
     pygame.draw.line(screen, 'blue',(400, 0), (400, 600), 10)
     pygame.draw.line(screen, 'blue',(0, 200), (600, 200), 10)
     pygame.draw.line(screen, 'blue',(0, 400), (600, 400), 10)
 
-    # primeira linha 
-    #                          x  y
-    if  x > 0 and x < 200 and y < 200:
-        screen.blit(personagem_x,(60,30)) #primeiro
-    elif x >= 200 and x < 400 and y <200:
-        screen.blit(personagem_y,(260,30)) #segundo
-    elif x >= 400 and y < 200:
-        screen.blit(personagem_y,(460,30)) #terceiro
+    if coordenada_x > 0 and coordenada_x < 200 and coordenada_y< 200:
+        screen.blit(jogador_atual,(60,30))  #primeiro
 
-    # segunda linha 
-    #                          x  y
-    if x < 200 and y >= 200 and y < 400:
-        screen.blit(personagem_x,(60,230)) #quarto
-    elif x >= 200 and x < 400 and y >= 200 and y < 400:
-        screen.blit(personagem_y,(260,230)) #quinto
-    elif x >= 400 and y >= 200 and y < 400:
-        screen.blit(personagem_y,(460,230)) #sexto
-
-    # terceira linha 
-    #                          x  y
-    if x < 200 and y >= 400 :
-        screen.blit(personagem_x,(60,430)) #setimo
-    elif x >= 200 and x < 400 and y >= 400:
-        screen.blit(personagem_y,(260,430)) #oitava
-    elif x >= 400 and y >+ 400:
-        screen.blit(personagem_y,(460,430)) #nono
+    elif coordenada_x >= 200 and coordenada_x < 400 and coordenada_y< 200:
+        screen.blit(jogador_atual,(260,30)) #segundo
     
+    elif coordenada_x >= 400 and coordenada_y< 200:
+        screen.blit(jogador_atual,(460,30)) #terceiro
+    
+    elif coordenada_x < 200 and coordenada_y>= 200 and coordenada_y< 400:
+        screen.blit(jogador_atual,(60,230))  #quarto
+    
+    elif coordenada_x >= 200 and coordenada_x < 400 and coordenada_y>= 200 and coordenada_y< 400:
+        screen.blit(jogador_atual,(260,230)) #quinto
+    
+    elif coordenada_x >= 400 and coordenada_y>= 200 and coordenada_y< 400:
+        screen.blit(jogador_atual,(460,230)) #secoordenada_xto
+    
+    elif coordenada_x < 200 and coordenada_y>= 400:
+        screen.blit(jogador_atual,(60,430))  #setimo
+    
+    elif coordenada_x >= 200 and coordenada_x < 400 and coordenada_y>= 400:
+        screen.blit(jogador_atual,(260,430)) #oitavo
+    
+    elif coordenada_x >= 400 and coordenada_y>= 400:
+        screen.blit(jogador_atual,(460,430)) #nono
+
+
 
     # flip() o display para atualizar a página
     pygame.display.flip()
